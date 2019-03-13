8c588bd6b2e21047ef45e6bb3148c1355ce35a0c
liu.yalei <liu.yalei@mwee.cn>
2019-03-13 15:47:30 +0800

    Common chunk

---
+++ demo12/bundle1.js
@@ -0,0 +1,20 @@
+webpackJsonp([1],{
+
+/***/ 7:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// main1.jsx
+var React = __webpack_require__(1);
+var ReactDOM = __webpack_require__(4);
+ReactDOM.render(React.createElement(
+    'h1',
+    null,
+    'Hello World'
+), document.getElementById('a'));
+
+/***/ })
+
+},[7]);
---
+++ demo12/main1.jsx
@@ -0,0 +1,7 @@
+// main1.jsx
+var React = require('react');
+var ReactDOM = require('react-dom');
+ReactDOM.render(
+    <h1>Hello World</h1>,
+    document.getElementById('a')
+);
