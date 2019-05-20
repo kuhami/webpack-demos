ff5e8338dc931a04ca65637b9803b3736a90a0e9
liu.yalei <liu.yalei@mwee.cn>
2019-05-20 15:52:05 +0800

    update:print

--- base-guides-demo/src/index.js
+++ base-guides-demo/src/index.js
@@ -1,11 +1,15 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
-
+    var btn = document.createElement('button');
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+    btn.innerHTML = '点击这里，然后查看 console！';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
