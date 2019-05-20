5db12015b456c372a4e4a3679a761d1a984f5d88
liu.yalei <liu.yalei@mwee.cn>
2019-05-20 16:59:27 +0800

    add HtmlWebpackPlugin

--- base-guides-demo/webpack.config.js
+++ base-guides-demo/webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ],
     output: {
         filename: '[name].bundle.js',
         // path: path.resolve(__dirname, 'dist')
